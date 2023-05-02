49df7e64ac4dcd24bdd0f3c9b5789562448a5ef6
realejyg <jessica.reale@ruhr-uni-bochum.de>
2023-05-02 14:49:28 +0200

    add sens plots

---
+++ src/model/sens-analysis/lib.jl
@@ -0,0 +1,77 @@
+function on_loans(df::DataFrame, param::Symbol)
+    fig = Figure(resolution = (800, 400))
+    ax = fig[1,1] = Axis(fig, title = "ON Loans", xlabel = "Steps", ylabel = "Average Volumes")
+    gdf = groupby(df, param)
+
+    for (key, subdf) in pairs(gdf)
+        _, trend = hp_filter((subdf.ON_assets), 1600)
+        lines!(trend; 
+            label = "$(param) = $(key[1])")
+    end
+    ax.xticks = 0:200:1200
+
+    fig[1, end+1] = Legend(fig, ax; 
+        orientation = :vertical, tellwidth = true)
+
+    return fig
+end
+
+function term_loans(df::DataFrame, param::Symbol)
+    fig = Figure(resolution = (800, 400))
+    ax = fig[1,1] = Axis(fig, title = "Term Loans", xlabel = "Steps", ylabel = "Average Volumes")
+    gdf = groupby(df, param)
+
+    for (key, subdf) in pairs(gdf)
+        _, trend = hp_filter((subdf.Term_assets), 1600)
+        lines!(trend; 
+            label = "$(param) = $(key[1])")
+        end
+    ax.xticks = 0:200:1200
+
+    fig[1, end+1] = Legend(fig, ax; 
+        orientation = :vertical, tellwidth = true)
+
+    return fig
+end
+
+function credit_loans(df::DataFrame, param::Symbol; f::Bool = true)
+    fig = Figure(resolution = (800, 400))
+    ax = fig[1,1] = Axis(fig, xlabel = "Steps", ylabel = "Average Volumes")
+    gdf = groupby(df, param)
+
+    for (key, subdf) in pairs(gdf)
+        _, trend = hp_filter((subdf.loans), 1600)
+        lines!(trend; 
+            label = "$(param) = $(key[1])")
+    end
+    ax.xticks = 0:200:1200
+
+    ax.title = if f 
+        "Firms Loans"
+        else
+            "Households Loans"
+        end
+
+    fig[1, end+1] = Legend(fig, ax; 
+        orientation = :vertical, tellwidth = true)
+
+    return fig
+end
+
+function output(df::DataFrame, param::Symbol)
+    fig = Figure(resolution = (800, 400))
+    ax = fig[1,1] = Axis(fig, title = "GDP", xlabel = "Steps", ylabel = "Average Volumes")
+    gdf = groupby(df, param)
+
+    for (key, subdf) in pairs(gdf)
+        _, trend = hp_filter((subdf.output), 1600)
+        lines!(trend; 
+            label = "$(param) = $(key[1])")
+    end
+    ax.xticks = 0:200:1200
+
+    fig[1, end+1] = Legend(fig, ax; 
+        orientation = :vertical, tellwidth = true)
+
+    return fig
+end
---
+++ src/model/sens-analysis/script.jl
@@ -0,0 +1,103 @@
+using Pkg
+
+# Load packages & code libraries
+using DataFrames
+using CSV
+using Pipe
+using Statistics
+using CairoMakie
+using QuantEcon
+
+include("lib.jl")
+
+function interbank(df::DataFrame, param::Symbol)
+    vars = [:ON_assets, :Term_assets]
+
+    # take only bank agents
+    df = @pipe df |> dropmissing(_, vars) |> groupby(_, [:step, param]) |> 
+        combine(_, vars .=> mean, renamecols = false)
+    
+    p = on_loans(df, param)
+    save("on_loans_sens.pdf", p)
+
+    p = term_loans(df, param)
+    save("term_loans_sens.pdf", p)
+
+end
+
+function credit(df::DataFrame, m::DataFrame, param::Symbol)
+    df_hh = @pipe df |> filter(:id => x -> x >= 1 && x <= mean(m[!, :n_hh]), _) |>
+        groupby(_, [:step, param]) |> 
+        combine(_, [:loans] .=> mean, renamecols = false)
+   
+    df_firms = @pipe df |>  filter(:id => x -> x > mean(m[!, :n_hh]) && x <= mean(m[!, :n_hh]) + mean(m[!, :n_f]), _) |>
+        groupby(_, [:step, param]) |> 
+        combine(_, [:loans, :output] .=> mean, renamecols = false)
+
+    p = credit_loans(df_firms, param)
+    save("loans_firms_sens.pdf", p)
+
+    p = output(df_firms, param)
+    save("output_sens.pdf", p)
+
+    p = credit_loans(df_hh, param; f = false)
+    save("loans_hh_sens.pdf", p)
+end
+
+function load_data()
+    # parameter names as strings
+    params_strings = ["r", "a0", "a1", "a2", "a3", "δ"]
+    # parameter ranges
+    params_range = ["0.9", "1.1", "1.3", "0.1", "0.5", "1.0", "0.2", "0.5", "1.0", "0.4", "0.5", "1.0", "0.05", "0.5", "1.0"]
+
+    df = DataFrame()
+    for param in params_strings
+        if param == "r"
+            for val in params_range[1:3]
+                append!(df, CSV.File("data/sensitivity_analysis/$(param)/$(val)/df.csv"); cols = :union)
+            end
+        elseif param in params_strings[2:2]
+            for val in params_range[4:6]
+                append!(df, CSV.File("data/sensitivity_analysis/$(param)/$(val)/df.csv"); cols = :union)
+            end
+        elseif param in params_strings[3:4]
+            for val in params_range[7:9]
+                append!(df, CSV.File("data/sensitivity_analysis/$(param)/$(val)/df.csv"); cols = :union)
+            end
+        elseif param in params_strings[5:5]
+            for val in params_range[10:12]
+                append!(df, CSV.File("data/sensitivity_analysis/$(param)/$(val)/df.csv"); cols = :union)
+            end
+        else
+            for val in params_range[13:end]
+                append!(df, CSV.File("data/sensitivity_analysis/$(param)/$(val)/df.csv"); cols = :union)
+            end
+        end
+    end
+
+    # take model variables from Baseline scenario
+    mdf = DataFrame()
+    append!(mdf, CSV.File("data/Baseline/mdf.csv"))
+
+    return df, mdf
+end
+
+function create_sens_plots()
+    df, mdf = load_data()
+
+    # parameter names as symbols
+    param_symbols = [:r, :a0, :a1, :a2, :a3, :δ]
+
+    cd(mkpath("img/pdf/sens-analysis")) do
+        for param in param_symbols
+            cd(mkpath("$(param)")) do
+                interbank(filter(param => x -> !ismissing(x), df), param)
+                credit(filter(param => x -> !ismissing(x), df), mdf, param)
+            end
+        end
+    end
+    printstyled("Sensitivity plots generated."; color = :blue)
+end
+
+create_sens_plots()
+ 
