31dedc91ac3527bfff6d4057f065703b0b4bb437
realejyg <jessica.reale@ruhr-uni-bochum.de>
2023-05-04 17:03:29 +0200

    add external server

--- src/run_complex.jl
+++ src/run_complex.jl
@@ -4,12 +4,16 @@ Pkg.instantiate()
 
 # Start workers
 using Distributed
-addprocs(5)
+addprocs([("ubuntu@ec2-13-53-139-133.eu-north-1.compute.amazonaws.com", :auto)];
+   exename = "/home/ubuntu/julia-1.8.5/bin/julia",
+    dir = "/home/ubuntu/projects/IMS",
+    tunnel = true)
 
 # Set up package environment on workers
 @everywhere begin
     using Pkg
     Pkg.activate(".")
+    #Pkg.instantiate()
 end
 
 # Load packages on master process
@@ -28,16 +32,17 @@ using IMS
 end
 
 # runs the model, transforms and saves data
-function run_model(number_of_runs::Int = 50)
+function run_model(number_of_runs::Int = 100)
     scenarios = ("Baseline", "Corridor", "Uncertainty", "Width")
-
+    
     # collect agent variables
     adata = [:type, :status, :ON_assets, :ON_liabs, :margin_stability, :am, :bm,
-        :Term_assets, :Term_liabs, :loans, :loans_prev, :output, :pmb, :pml,
+        :Term_assets, :Term_liabs, :loans, :loans_prev, :output, :pmb, :pml, :tot_assets, 
+        :tot_liabilities,
         :il_rate, :id_rate, :funding_costs, :lending_facility, :deposit_facility,
         :on_demand, :term_demand, :on_supply, :term_supply, :prices]
     # collect model variables
-    mdata = [:n_hh, :n_f, :IBon, :IBterm, :ion, :iterm, :icbl, :icbd, :icbt, :θ, :LbW]
+    mdata = [:n_hh, :n_f, :ion, :iterm, :icbl, :icbd, :icbt, :θ, :LbW]
 
     for scenario in scenarios
         seeds = rand(UInt32, number_of_runs)
@@ -47,7 +52,7 @@ function run_model(number_of_runs::Int = 50)
         models = [IMS.init_model(; seed, scenario = scenario) for seed in seeds]
         
         adf, mdf, _ =  ensemblerun!(models, dummystep, IMS.model_step!, 1200;
-            adata, mdata, parallel = true, showprogress = true)
+            adata, mdata, parallel = false, showprogress = true)
             
         println("Collecting data for $(scenario)-scenario...")
 
@@ -59,7 +64,7 @@ function run_model(number_of_runs::Int = 50)
 
         # Aggregate agent data over replicates
         adf = @pipe adf |>
-            groupby(_, [:step, :id, :status]) |>
+            groupby(_, [:step, :id, :status, :type]) |>
             combine(_, adata[1:2] .=> unique, adata[3:end] .=> mean; renamecols = false)
         adf[!, :scenario] = fill(scenario, nrow(adf))
 
