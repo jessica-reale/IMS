f45d4820a4b6cf8ac908dbf51279cdbcdd5d0766
realejyg <jessica.reale@ruhr-uni-bochum.de>
2023-05-23 11:38:37 +0200

    condensed function plots

--- src/plots/lib.jl
+++ src/plots/lib.jl
@@ -3,6 +3,17 @@ const BANKS_TYPE_LABELS = ("Business", "Commercial")
 const IB_STATUS = ("deficit", "surplus")
 const IB_LABELS = ("Deficit", "Surplus")
 
+function plots_variables(fig, axes, gdf, vars)
+    for i in 1:length(vars.variables)   
+        ax = fig[axes[i]...] = Axis(fig, title = vars.labels[i])
+        for j in 2:length(gdf)
+            _, trend = hp_filter((((gdf[j][!, vars.variables[i]][100:end] .- gdf[1][!, vars.variables[i]][100:end])) ./ gdf[1][!, vars.variables[i]][100:end]) .* 100, 129600)
+            lines!(movavg(trend, 200).x; color = Makie.wong_colors()[j] ,  label = only(unique(gdf[j].shock)))
+        end
+        ax.xticks = (collect(100:200:1200), ["200", "400", "600", "800", "1000", "1200"])
+    end
+end
+
 # Big plots 
 function big_credit_firms_plots(df)
     fig = Figure(resolution = (700, 250), fontsize = 10)
@@ -10,19 +21,10 @@ function big_credit_firms_plots(df)
     gdf = @pipe df |> 
         groupby(_, :shock)
 
-    colors = Makie.wong_colors() # excludes blue used for "Missing" shock    
-    
     vars = (variables = [:loans, :output], 
         labels = ["Loans", "Output"])   
             
-    for i in 1:length(vars.variables)   
-        ax = fig[axes[i]...] = Axis(fig, title = vars.labels[i])
-        for j in 2:length(gdf)
-            _, trend = hp_filter((((gdf[j][!, vars.variables[i]][100:end] .- gdf[1][!, vars.variables[i]][100:end])) ./ gdf[1][!, vars.variables[i]][100:end]) .* 100, 129600)
-            lines!(movavg(trend, 200).x; color = colors[j],  label = only(unique(gdf[j].shock)))
-        end
-        ax.xticks = (collect(100:200:1200), ["200", "400", "600", "800", "1000", "1200"])
-    end
+    plots_variables(fig, axes, gdf, vars)
 
     ax1 = fig.content[1]; ax2 = fig.content[2]
 
@@ -45,22 +47,13 @@ function big_credit_hh_plots(df)
     gdf = @pipe df |> 
         groupby(_, :shock)
 
-    colors = Makie.wong_colors() # excludes blue used for "Missing" shock    
-
     vars = (variables = [:loans, :consumption], 
         labels = ["Loans", "Consumption"])   
             
-    for i in 1:length(vars.variables)   
-        ax = fig[axes[i]...] = Axis(fig, title = vars.labels[i])
-        for j in 2:length(gdf)
-            _, trend = hp_filter((((gdf[j][!, vars.variables[i]][100:end] .- gdf[1][!, vars.variables[i]][100:end])) ./ gdf[1][!, vars.variables[i]][100:end]) .* 100, 129600)
-            lines!(movavg(trend, 200).x; color = colors[j], label = only(unique(gdf[j].shock)))
-        end
-        ax.xticks = (collect(100:200:1200), ["200", "400", "600", "800", "1000", "1200"])
-    end
+    plots_variables(fig, axes, gdf, vars)
 
     ax1 = fig.content[1]; ax2 = fig.content[2]
-    ax1.ylabel =  "Mean"
+    ax1.ylabel =  "Moving Average"
     ax1.xlabel = ax2.xlabel = "Steps"
 
     fig[end+1,1:2] = Legend(fig, 
@@ -78,20 +71,11 @@ function big_ib_plots(df)
     gdf = @pipe df |> 
         groupby(_, :shock)
     
-    colors = Makie.wong_colors() # excludes blue used for "Missing" shock    
-    
     vars = (variables = [:ON_liabs, :Term_liabs, :deposit_facility, :lending_facility, :margin_stability, :am, :bm, :pmb, :pml], 
         labels = ["Overnight segment", "Term segment", "Deposit facility", "Lending Facility", "Margin of stability", "ASF", "RSF", 
             L"\Pi^{b}", L"\Pi^{l}"])   
             
-    for i in 1:length(vars.variables)   
-        ax = fig[axes[i]...] = Axis(fig, title = vars.labels[i])
-        for j in 2:length(gdf)
-            _, trend = hp_filter(((gdf[j][!, vars.variables[i]][100:end] .- gdf[1][!, vars.variables[i]][100:end])./gdf[1][!, vars.variables[i]][100:end]) .* 100, 129600)
-            lines!(movavg(trend, 200).x; color = colors[j], label = only(unique(gdf[j].shock)))
-        end
-        ax.xticks = (collect(100:200:1200), ["200", "400", "600", "800", "1000", "1200"])
-    end
+    plots_variables(fig, axes, gdf, vars)
 
     ax1 = fig.content[1]; ax2 = fig.content[2]; ax3 = fig.content[3];
     ax4 = fig.content[4]; ax5 = fig.content[5]; ax6 = fig.content[6];
@@ -124,20 +108,13 @@ function big_ib_plots_levels(df)
         labels = ["Overnight segment", "Term segment", "Deposit facility", "Lending Facility", "Margin of stability", "ASF", "RSF", 
             L"\Pi^{b}", L"\Pi^{l}"])   
             
-    for i in 1:length(vars.variables)   
-        ax = fig[axes[i]...] = Axis(fig, title = vars.labels[i])
-        for j in 1:length(gdf)
-            _, trend = hp_filter((gdf[j][!, vars.variables[i]][100:end]), 129600)
-            lines!(movavg(trend, 200).x; label = only(unique(gdf[j].shock)))
-        end
-        ax.xticks = (collect(100:200:1200), ["200", "400", "600", "800", "1000", "1200"])
-    end
+    plots_variables(fig, axes, gdf, vars)
 
     ax1 = fig.content[1]; ax2 = fig.content[2]; ax3 = fig.content[3];
     ax4 = fig.content[4]; ax5 = fig.content[5]; ax6 = fig.content[6];
     ax7 = fig.content[7]; ax8 = fig.content[8]; ax9 = fig.content[9]
 
-    ax1.ylabel = ax4.ylabel = ax7.ylabel = "Mean"
+    ax1.ylabel = ax4.ylabel = ax7.ylabel = "Moving Average"
     ax7.xlabel = ax8.xlabel = ax9.xlabel = "Steps"
     ax1.xticklabelsvisible = ax2.xticklabelsvisible = ax3.xticklabelsvisible = 
         ax4.xticklabelsvisible = ax5.xticklabelsvisible = ax6.xticklabelsvisible = false
@@ -158,21 +135,12 @@ function big_ib_baseline_plots(df)
     fig = Figure(resolution = (1250, 250), fontsize = 10)
     axes = ((1,1), (1,2), (1,3), (1,4))
     gdf = @pipe df |> 
-        groupby(_, :shock)
-    
-    colors = Makie.wong_colors() # excludes blue used for "Missing" shock    
+        groupby(_, :shock)   
 
     vars = (variables = [:ON_liabs, :Term_liabs, :deposit_facility, :lending_facility], 
         labels = ["Overnight segment", "Term segment", "Deposit facility", "Lending Facility"])   
             
-    for i in 1:length(vars.variables)   
-        ax = fig[axes[i]...] = Axis(fig, title = vars.labels[i])
-        for j in 2:length(gdf)
-            _, trend = hp_filter(((gdf[j][!, vars.variables[i]][100:end] .- gdf[1][!, vars.variables[i]][100:end])./gdf[1][!, vars.variables[i]][100:end]) .* 100, 129600)
-            lines!(movavg(trend, 200).x; color = colors[j], label = only(unique(gdf[j].shock)))
-        end
-        ax.xticks = (collect(100:200:1200), ["200", "400", "600", "800", "1000", "1200"])
-    end
+    plots_variables(fig, axes, gdf, vars)
 
     ax1 = fig.content[1]; ax2 = fig.content[2]; ax3 = fig.content[3];  ax4 = fig.content[4]; 
    
@@ -198,19 +166,12 @@ function big_ib_baseline_plots_levels(df)
     vars = (variables = [:ON_liabs, :Term_liabs, :deposit_facility, :lending_facility], 
         labels = ["Overnight segment", "Term segment", "Deposit facility", "Lending Facility"])   
             
-    for i in 1:length(vars.variables)   
-        ax = fig[axes[i]...] = Axis(fig, title = vars.labels[i])
-        for j in 1:length(gdf)
-            _, trend = hp_filter((gdf[j][!, vars.variables[i]][100:end]), 129600)
-            lines!(movavg(trend, 200).x; label = only(unique(gdf[j].shock)))
-        end
-        ax.xticks = (collect(100:200:1200), ["200", "400", "600", "800", "1000", "1200"])
-    end
+    plots_variables(fig, axes, gdf, vars)
 
     ax1 = fig.content[1]; ax2 = fig.content[2]; ax3 = fig.content[3];  ax4 = fig.content[4]; 
    
 
-    ax1.ylabel = "Mean"
+    ax1.ylabel = "Moving Average"
     ax1.xlabel = ax2.xlabel = ax3.xlabel = ax4.xlabel = "Steps"
    
     fig[end+1,1:4] = Legend(fig, 
@@ -242,7 +203,7 @@ function big_rationing_plot(df)
 
     ax1 = fig.content[1]; ax2 = fig.content[2]
     
-    ax1.ylabel = "Mean"
+    ax1.ylabel = "Moving Average"
     ax1.xlabel = ax2.xlabel = "Steps"
     linkyaxes!(fig.content...)
     ax2.yticklabelsvisible = false
@@ -263,23 +224,14 @@ function big_ib_by_status(df)
     gdf = @pipe df |> 
         groupby(_, :shock)
 
-    colors = Makie.wong_colors() # excludes blue used for "Missing" shock    
-
     vars = (variables = [:margin_stability, :am, :bm], 
         labels = ["Margin of stability", "ASF", "RSF"])   
         
-    for i in 1:length(vars.variables)   
-        ax = fig[axes[i]...] = Axis(fig, title = vars.labels[i])
-        for j in 2:length(gdf)
-            _, trend = hp_filter((((gdf[j][!, vars.variables[i]][100:end] .- gdf[1][!, vars.variables[i]][100:end])) ./ gdf[1][!, vars.variables[i]][100:end]) .* 100, 129600)
-            lines!(movavg(trend, 200).x; color = colors[j], label = only(unique(gdf[j].shock)))
-        end
-        ax.xticks = (collect(100:200:1200), ["200", "400", "600", "800", "1000", "1200"])
-    end
+    plots_variables(fig, axes, gdf, vars)
 
     ax1 = fig.content[1]; ax2 = fig.content[2]; ax3 = fig.content[3];
 
-    ax1.ylabel =  "Mean"
+    ax1.ylabel =  "Moving Average"
     ax1.xlabel = ax2.xlabel = ax3.xlabel = "Steps"
 
     fig[end+1,1:3] = Legend(fig, 
@@ -297,19 +249,10 @@ function theta_lbw(df)
     gdf = @pipe df |> 
         groupby(_, :shock)
 
-    colors = Makie.wong_colors() # excludes blue used for "Missing" shock    
-
     vars = (variables = [:θ, :LbW], 
         labels = [L"θ", L"L_{b}W"])   
             
-    for i in 1:length(vars.variables)   
-        ax = fig[axes[i]...] = Axis(fig, title = vars.labels[i])
-        for j in 2:length(gdf)
-            _, trend = hp_filter((((gdf[j][!, vars.variables[i]][100:end] .- gdf[1][!, vars.variables[i]][100:end])) ./ gdf[1][!, vars.variables[i]][100:end]) .* 100, 129600)
-            lines!(movavg(trend, 200).x; color = colors[j], label = only(unique(gdf[j].shock)))
-        end
-        ax.xticks = (collect(100:200:1200), ["200", "400", "600", "800", "1000", "1200"])
-    end
+    plots_variables(fig, axes, gdf, vars)
 
     ax1 = fig.content[1]; ax2 = fig.content[2]
 
@@ -331,20 +274,11 @@ function interest_ib(df)
     axes = ((1,1), (1,2))
     gdf = @pipe df |> 
         groupby(_, :shock)
-    
-    colors = Makie.wong_colors() # excludes blue used for "Missing" shock    
 
     vars = (variables = [:ion, :iterm], 
         labels = ["ON rate", "Term rate"])   
             
-    for i in 1:length(vars.variables)   
-        ax = fig[axes[i]...] = Axis(fig, title = vars.labels[i])
-        for j in 2:length(gdf)
-            _, trend = hp_filter((((gdf[j][!, vars.variables[i]][100:end] .- gdf[1][!, vars.variables[i]][100:end])) ./ gdf[1][!, vars.variables[i]][100:end]) .* 100, 129600)
-            lines!(movavg(trend, 200).x; color = colors[j], label = only(unique(gdf[j].shock)))
-        end
-        ax.xticks = (collect(100:200:1200), ["200", "400", "600", "800", "1000", "1200"])
-    end
+    plots_variables(fig, axes, gdf, vars)
 
     ax1 = fig.content[1]; ax2 = fig.content[2]
 
@@ -382,7 +316,7 @@ function flows_by_status_levels(df)
     by_status(fig, axes, gdf, :flow)
 
     ax1 = fig.content[1]; ax2 = fig.content[2]
-    ax1.ylabel = ax2.ylabel = "Mean"
+    ax1.ylabel = ax2.ylabel = "Moving Average"
     ax1.xlabel = ax2.xlabel  = "Steps"
 
     fig[end+1,1:2] = Legend(fig, 
@@ -404,7 +338,7 @@ function stability_by_status_levels(df)
     by_status(fig, axes, gdf, :margin_stability)
 
     ax1 = fig.content[1]; ax2 = fig.content[2]
-    ax1.ylabel = ax2.ylabel = "Mean"
+    ax1.ylabel = ax2.ylabel = "Moving Average"
     ax1.xlabel = ax2.xlabel  = "Steps"
 
     fig[end+1,1:2] = Legend(fig, 
@@ -438,7 +372,7 @@ function flows_by_type_levels(df)
     by_type(fig, axes, gdf, :flow)
 
     ax1 = fig.content[1]; ax2 = fig.content[2]
-    ax1.ylabel = ax2.ylabel = "Mean"
+    ax1.ylabel = ax2.ylabel = "Moving Average"
     ax1.xlabel = ax2.xlabel  = "Steps"
 
     fig[end+1,1:2] = Legend(fig, 
@@ -460,7 +394,7 @@ function stability_by_type_levels(df)
     by_type(fig, axes, gdf, :margin_stability)
 
     ax1 = fig.content[1]; ax2 = fig.content[2]
-    ax1.ylabel = ax2.ylabel = "Mean"
+    ax1.ylabel = ax2.ylabel = "Moving Average"
     ax1.xlabel = ax2.xlabel  = "Steps"
 
     fig[end+1,1:2] = Legend(fig, 
@@ -482,7 +416,7 @@ function credit_rates_by_type_levels(df)
     by_type(fig, axes, gdf, :il_rate)
 
     ax1 = fig.content[1]; ax2 = fig.content[2]
-    ax1.ylabel = ax2.ylabel = "Mean"
+    ax1.ylabel = ax2.ylabel = "Moving Average"
     ax1.xlabel = ax2.xlabel  = "Steps"
 
     fig[end+1,1:2] = Legend(fig, 
