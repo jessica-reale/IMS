e9abd60dfe700bd49d414afd85575d7bf2c9f44c
realejyg <jessica.reale@ruhr-uni-bochum.de>
2023-11-13 15:10:48 +0100

    add seeds run sensitivity analysis

--- src/sens-analysis/run.jl
+++ src/sens-analysis/run.jl
@@ -13,12 +13,9 @@ using Distributions
 using IMS
 
 # runs the sensitivity analysis for the parameters of interests, transforms and collects datas
-function run_sens(param::Symbol, param_range::Vector{Float64}; scenario::String = "Baseline")
+function run_sens(seeds::Vector{UInt32}, param::Symbol, param_range::Vector{Float64}; scenario::String = "Baseline")
     # collect agent variables
-    adata = [:type, :status, :ib_flag, :margin_stability, :am, :bm, :flow,
-        :lending_facility, :deposit_facility, :on_demand, :term_demand,
-        :loans, :output, :pmb, :pml, :il_rate, :id_rate, :ON_liabs, :Term_liabs,
-        :consumption]
+    adata = [:type, :status, :ib_flag, :margin_stability, :output, :ON_liabs, :Term_liabs]
 
     for x in param_range
         # Setup model properties
@@ -29,14 +26,16 @@ function run_sens(param::Symbol, param_range::Vector{Float64}; scenario::String 
 
         println("Running parameter scans for $(param) at $(x)...")
         
-        df, _ = paramscan(properties, IMS.init_model; 
-                adata = adata, model_step! = IMS.model_step!, n = 1200,
-                include_constants = true)
+        models = [IMS.init_model(; seed, properties...) for seed in seeds]
+
+        # run parallel models
+        adf, _ =  ensemblerun!(models, dummystep, IMS.model_step!, 1200;
+            adata, parallel = true, showprogress = true)
 
         println("Collecting data for $(param) at $(x)...")
 
         # Aggregate agent data
-        df = @pipe df |>
+        adf = @pipe adf |>
             groupby(_, [param, :step, :id, :status]) |>
             combine(_, adata[1:3] .=> unique, adata[4:end] .=> mean; renamecols = false)
 
@@ -45,28 +44,32 @@ function run_sens(param::Symbol, param_range::Vector{Float64}; scenario::String 
         datapath = mkpath("data/sensitivity_analysis/$(param)/$(x)")
         filepath = "$datapath/df.csv"
         isfile(filepath) && rm(filepath)
-        CSV.write(filepath, df)
+        CSV.write(filepath, adf)
+        empty!(adf)
     end
     return nothing
 end
 
-function run()
+function run(seeds::Vector{UInt32})
     # general params
-    run_sens(:r, [0.9, 1.1, 1.3])
-    run_sens(:δ, [0.05, 0.5, 1.0])
-    run_sens(:l , [0.03, 0.5, 1.0])
-    run_sens(:γ, [0.1, 0.5, 1.0])
-    run_sens(:gd, [0.1, 0.5, 1.0])
+    run_sens(seeds, :r, [0.9, 1.1, 1.3])
+    run_sens(seeds, :δ, [0.05, 0.5, 1.0])
+    run_sens(seeds, :l , [0.03, 0.5, 1.0])
+    run_sens(seeds, :γ, [0.1, 0.5, 1.0])
+    run_sens(seeds, :gd, [0.1, 0.5, 1.0])
     # NSFR params
-    run_sens(:m1, collect(0.0:0.1:1.0); scenario = "Maturity")
-    run_sens(:m2, collect(0.0:0.1:1.0); scenario = "Maturity")
-    run_sens(:m3, collect(0.0:0.1:1.0); scenario = "Maturity")
-    run_sens(:m4, collect(0.0:0.1:1.0); scenario = "Maturity")
-    run_sens(:m5, collect(0.0:0.1:1.0); scenario = "Maturity")
+    run_sens(seeds, :m1, collect(0.0:0.1:1.0); scenario = "Maturity")
+    run_sens(seeds, :m2, collect(0.0:0.1:1.0); scenario = "Maturity")
+    run_sens(seeds, :m3, collect(0.0:0.1:1.0); scenario = "Maturity")
+    run_sens(seeds, :m4, collect(0.0:0.1:1.0); scenario = "Maturity")
+    run_sens(seeds, :m5, collect(0.0:0.1:1.0); scenario = "Maturity")
    
     printstyled("Paramascan and data collection finished."; color = :blue)
     return nothing
 end
 
-Random.seed!(96100)
-run()
+begin 
+    Random.seed!(96100)
+    seeds = rand(UInt32, 100)
+    run(seeds)
+end
